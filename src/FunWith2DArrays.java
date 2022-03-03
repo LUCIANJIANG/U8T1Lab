a5db7ad748e91ae32f61922a92d24e597a43a456
D <dannyc.88ninja@gmail.com>
2022-03-03 13:40:13 -0500

    saved changes

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -15,4 +15,41 @@ public class FunWith2DArrays {
         System.out.println(strArr[rows - 1][0]);
         System.out.println(strArr[rows - 1][columns - 1]);
     }
+
+    public static double average(int[][] numArr)
+    {
+        int count = 0;
+        int total = 0;
+        for (int[] row : numArr)
+        {
+            for (int element : row)
+            {
+                total += element;
+                count++;
+            }
+        }
+        return (double) total / count;
+    }
+
+    public static int[] indexFound(String[][] strArr, String target)
+    {
+        int[] found = new int[2];
+        for (int row = 0; row < strArr.length; row++)
+        {
+            for (int col = 0; col < strArr[0].length; col++)
+            {
+                String element = strArr[row][col];
+                if (element.equals(target))
+                {
+                    found[0] = row;
+                    found[1] = col;
+
+                    return found;
+                }
+            }
+        }
+        found[0] = -1;
+        found[1] = -1;
+        return found;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -86,28 +86,54 @@ public class Main
 //        int[][] testArr9 = {{}};
 //        System.out.println(FunWith2DArrays.totalElements(testArr9));
 
-        String[][] words = {{"hi", "bye", "stuff", "go"},
-                           {"time", "up", "you", "good"},
-                           {"map", "low", "bow", "mom"}};
-        FunWith2DArrays.fourCorners(words);
+//        String[][] words = {{"hi", "bye", "stuff", "go"},
+//                           {"time", "up", "you", "good"},
+//                           {"map", "low", "bow", "mom"}};
+//        FunWith2DArrays.fourCorners(words);
+//
+//        String[][] words2 = {{"time", "up", "bye"},
+//                            {"hi", "hit", "up"},
+//                            {"map", "bam", "low"},
+//                            {"bow", "mom", "joy"}};
+//        FunWith2DArrays.fourCorners(words2);
+//
+//        String[][] words3 = {{"phone", "mouse", "keyboard"}};
+//        FunWith2DArrays.fourCorners(words3);
+//
+//        String[][] words4 = {{"cat"},
+//                            {"dog"},
+//                            {"hamster"},
+//                            {"bird"}};
+//        FunWith2DArrays.fourCorners(words4);
+//
+//        String[][] words5 = {{"time"}};
+//        FunWith2DArrays.fourCorners(words5);
 
-        String[][] words2 = {{"time", "up", "bye"},
-                            {"hi", "hit", "up"},
-                            {"map", "bam", "low"},
-                            {"bow", "mom", "joy"}};
-        FunWith2DArrays.fourCorners(words2);
+        // TEST CODE FOR average
+//        int[][] testArr1 = {{1, 2, 3}, {4, 5, 6}};
+//        System.out.println(FunWith2DArrays.average(testArr1));
+//        int[][] testArr2 = {{1, 2, 3, 4}, {7, 8, 9, 10}};
+//        System.out.println(FunWith2DArrays.average(testArr2));
+//        int[][] testArr3 = {{4, 5, 2}, {1, 9, 7}, {8, 10, 13}};
+//        System.out.println(FunWith2DArrays.average(testArr3));
+//        int[][] testArr4 = {{4}, {7}, {8}, {2}};
+//        System.out.println(FunWith2DArrays.average(testArr4));
+//        int[][] testArr5 = {{4}};
+//        System.out.println(FunWith2DArrays.average(testArr5));
 
-        String[][] words3 = {{"phone", "mouse", "keyboard"}};
-        FunWith2DArrays.fourCorners(words3);
+        // TEST CODE FOR indexFound
+        String[][] words = {{"hi", "bye", "stuff", "go"}, {"time", "up", "you", "good"}, {"map", "low", "bow", "mom"}};
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "stuff")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "good")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "map")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "cat")));
+        System.out.println("--------------");
 
-        String[][] words4 = {{"cat"},
-                            {"dog"},
-                            {"hamster"},
-                            {"bird"}};
-        FunWith2DArrays.fourCorners(words4);
-
-        String[][] words5 = {{"time"}};
-        FunWith2DArrays.fourCorners(words5);
+        String[][] words2 = {{"time", "up", "bye"}, {"hi", "hit", "up"}, {"map", "bam", "low"}, {"bow", "mom", "joy"}};
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "time")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "joy")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "bam")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "cat")));
     }
 }
 
